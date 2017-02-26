a429ffc0145e37a9e85b9d76bb9b7f49c0078d38
Mohamed Afrar <mmafrar@gmail.com>
2017-02-26 11:45:43 +0530

    Added Home and Submit Article pages for the web application

--- web-app/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Get home page
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'e-Citizen' });
-});
-
-module.exports = router;
